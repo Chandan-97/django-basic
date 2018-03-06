fef79afda50cfe98cc0c681d3f9ab35b7785edf1
Chandan Prasad <cp564738@gmail.com>
2018-03-06 20:08:27 +0530

    ListView Search and dynamic links (18)

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,11 +1,12 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic.base import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from .models import RestaurantLocation
 
-def restuarant_listview(request):
+def restaurants_listview(request):
 	template_name = 'restaurants/restaurants_list.html'
 	queryset = RestaurantLocation.objects.all()
 	context = {
@@ -14,3 +15,32 @@ def restuarant_listview(request):
 	return render(request, template_name, context)
 
 
+class RestaurantListView(ListView):
+	def get_queryset(self):
+		# print(self.kwargs)
+		slug = self.kwargs.get("slug")
+		if(slug):
+			queryset = RestaurantLocation.objects.filter(
+				Q(category__iexact=slug) | Q(category__icontains=slug)
+			)
+		else:
+			queryset = RestaurantLocation.objects.all()
+		return queryset
+
+# class SearchRestaurantListView(ListView):
+# 	template_name = 'restaurants/restaurants_list.html'
+
+# 	def get_queryset(self):
+# 		# print(self.kwargs)
+# 		slug = self.kwargs.get("slug")
+# 		if(slug):
+# 			queryset = RestaurantLocation.objects.filter(
+# 				Q(category__iexact=slug) | Q(category__icontains=slug)
+# 			)
+# 		else:
+# 			queryset = RestaurantLocation.objects.none()
+# 		return queryset
+
+# class AsianFusionRestaurantListView(ListView):
+# 	queryset = RestaurantLocation.objects.filter(category__iexact='asianfusion')
+# 	template_name = 'restaurants/restaurants_list.html'
